a9ec4aaefb2fa58a81e1790d87b2f8861882602d
TheSyg <nlamontanara@gmail.com>
2023-06-14 00:17:02 -0300

    sexo

---
+++ TP6/puntoDieciocho.java
@@ -0,0 +1,19 @@
+package TP6;
+
+public class puntoDieciocho {
+    public static String explotar(int n, int b) {
+        String n1 = "";
+        String n2 = "";
+
+        if (n > b) {
+            n1 = explotar(n/b, b);
+            n2 = explotar((n - (n/b)), b);
+        }
+
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(explotar(21, 3));
+    }
+}
